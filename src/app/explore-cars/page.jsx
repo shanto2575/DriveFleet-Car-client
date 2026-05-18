6249731523f3bbd46e3b5096355e58a516d1f0a2
shanto2575 <shanto2575@gmail.com>
2026-05-18 11:40:37 +0600

    explore cars addd

--- src/app/explore-cars/page.jsx
+++ src/app/explore-cars/page.jsx
@@ -1,29 +1,29 @@
-// import CarsCardPage from '@/Components/CarsCard';
-// import React from 'react'
+import CarsCardPage from '@/Components/CarsCard';
+import React from 'react'
 
-// const ExploreCarsPage = async () => {
-//     const res = await fetch(`http://localhost:5000/cars`)
-//     const cars = await res.json()
-//     // console.log(cars)
+const ExploreCarsPage = async () => {
+    const res = await fetch(`http://localhost:5000/cars`)
+    const cars = await res.json()
+    // console.log(cars)
 
-//     return (
-//         <div className='w-10/12 mx-auto'>
-//             <div className="text-center mb-12 space-y-3">
-//                 <h1 className="text-4xl lg:text-5xl font-bold">
-//                     Explore Our <span className='text-orange-500'>Cars</span>
-//                 </h1>
-//                 <p className="text-gray-500 max-w-2xl mx-auto">
-//                     Choose from a wide range of vehicles for your next trip.
-//                     Find the perfect ride that fits your style and budget.
-//                 </p>
-//             </div>
-//             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-2'>
-//                 {
-//                     cars.map(cars => <CarsCardPage key={cars._id} cars={cars} />)
-//                 }
-//             </div>
-//         </div>
-//     )
-// }
+    return (
+        <div className='w-10/12 mx-auto'>
+            <div className="text-center mb-12 space-y-3">
+                <h1 className="text-4xl lg:text-5xl font-bold">
+                    Explore Our <span className='text-orange-500'>Cars</span>
+                </h1>
+                <p className="text-gray-500 max-w-2xl mx-auto">
+                    Choose from a wide range of vehicles for your next trip.
+                    Find the perfect ride that fits your style and budget.
+                </p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-2'>
+                {
+                    cars.map(cars => <CarsCardPage key={cars._id} cars={cars} />)
+                }
+            </div>
+        </div>
+    )
+}
 
-// export default ExploreCarsPage
+export default ExploreCarsPage
